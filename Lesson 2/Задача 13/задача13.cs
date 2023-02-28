ea24de538e425e317a7ae89eafaf056fdbef9599
TatyanaProtas <tatyana.protas.89@gmail.com>
2023-02-28 13:33:02 +0300

    выводит 2ю цифру, если она есть

--- Lesson 2/Задача 13/задача13.cs
+++ Lesson 2/Задача 13/задача13.cs
@@ -9,14 +9,12 @@ int number = ReadInt("Введите число: ");
 int count = number.ToString().Length;
 Console.Write(MakeArray(number, count));
 
-// Функция принимает сообщение для отображения в консоли, и выводит результат введенных данных пользователем.
 int ReadInt(string message)
 {
     Console.Write(message);
     return Convert.ToInt32(Console.ReadLine());
 }
 
-// Функция принимает число введенное пользователем, количество символов, и выводит третью цифру числа. Если 3 цифры нет, сообщает и выводит 0.
 int MakeArray(int a, int b)
 {
 int result = 0;
